12f5f3950c49b5adfa96a74a4dc00ce36df2475d
Forrest Miller <Forrestmillerj@gmail.com>
2020-03-27 16:58:18 -0500

    created connection.js file and orm.js file

---
+++ config/connection.js
@@ -0,0 +1,21 @@
+const mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "527996",
+    database: "burgers_db"
+});
+
+// make connection
+connection.connect(function(err){
+    if (err) {
+        console.lof("error connection: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+// exoport connection for ORM use.
+module.exports = connection;
