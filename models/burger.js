7a7fcb32c10e627a3e5a61a4fe4c3fa4e8f21f2e
Forrest Miller <Forrestmillerj@gmail.com>
2020-03-27 17:20:42 -0500

    all() create() and update() created.

--- models/burger.js
+++ models/burger.js
@@ -1,2 +1,22 @@
 const orm = require("../config/orm");
 
+var burger ={
+	all: function(cb) {
+			orm.selectAll("burgers", function(res) {
+					cb(res);
+			});
+	},
+	create: function(cols, vals, cb) {
+		orm.create("burgers", cols, vals, function(res) {
+			cb(res);
+		});
+	},
+	update: function(objColVals, conditon, cb) {
+		orm.update("burgers", objColVals, conditon, function(res) {
+			cb(res);
+		});
+	}
+};
+
+// Export the database functions for the controller
+module.exports = burger;
