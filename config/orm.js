8c47ec39824064cfd670b56b925cc6f4010f0c3d
Forrest Miller <Forrestmillerj@gmail.com>
2020-03-28 14:09:31 -0500

    added button to delete burger after being eaten

--- config/orm.js
+++ config/orm.js
@@ -63,9 +63,9 @@ var orm = {
 	updateOne: function(table, objColVals, condition, cb) {
 		var queryString = "UPDATE " + table;
 
-    queryString += " SET ";
-    queryString += objToSql(objColVals);
-    queryString += " WHERE ";
+		queryString += " SET ";
+		queryString += objToSql(objColVals);
+		queryString += " WHERE ";
 		queryString += condition;
 		
 		console.log(queryString);
@@ -76,6 +76,20 @@ var orm = {
 			cb(result);
 		});
 	},
+	removeOne: function(table, condition, cb) {
+		var queryString = "DELETE FROM " + table;
+
+		queryString += " WHERE ";
+		queryString += condition;
+		
+		console.log(queryString);
+		connection.query(queryString, function(err,result) {
+			if (err) {
+				throw err;
+			}
+			cb(result);
+		});
+	}
 }
 
 module.exports = orm;
